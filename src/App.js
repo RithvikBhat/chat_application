c70dfa443e3d9a9965f5143d10a516b2c6d812c7
Rithvik <code.rithvikkbhat@gmail.com>
2021-08-27 21:41:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="29ca6230-20d1-46e8-8789-109649c252a1"
+            userName="erenjaegar"
+            // Password
+            userSecret="123123"
+        />
+    )
+}
+
+export default App;
